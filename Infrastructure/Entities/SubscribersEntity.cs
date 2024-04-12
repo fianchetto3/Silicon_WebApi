77e9af247d267d80cfbcc4be31226ffa6a54846f
simon <simonelenbrink@gmail.com>
2024-04-12 15:03:43 +0200

    Add project files.

---
+++ Infrastructure/Entities/SubscribersEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class SubscribersEntity
+{
+    [Key]
+    [Required]
+    public string Email { get; set; } = null!;
+
+}
