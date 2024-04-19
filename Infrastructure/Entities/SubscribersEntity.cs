0ff5574a2e69b21432d62760433a8b300718bd54
simon <simonelenbrink@gmail.com>
2024-04-20 01:31:53 +0200

    Api finished

--- Infrastructure/Entities/SubscribersEntity.cs
+++ Infrastructure/Entities/SubscribersEntity.cs
@@ -4,8 +4,19 @@ namespace Infrastructure.Entities;
 
 public class SubscribersEntity
 {
+
     [Key]
-    [Required]
     public string Email { get; set; } = null!;
 
+    public bool DailyNewsLetter { get; set; }
+
+    public bool AdervtisingUpdates { get; set; }
+
+    public bool WeekInReview { get; set; }
+
+    public bool EventUpdates { get; set; }
+
+    public bool StartupsWeekly { get; set; }
+
+    public bool Podcasts { get; set; }
 }
